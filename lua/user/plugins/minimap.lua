ad2f6181ff7b52295ae0038a136b665518397aa6
Anton Kesy <antonkesy@gmail.com>
2024-10-09 21:42:09 +0200

    disable minimap

--- lua/user/plugins/minimap.lua
+++ lua/user/plugins/minimap.lua
@@ -3,8 +3,8 @@ return {
     "wfxr/minimap.vim",
     build = "cargo install --locked code-minimap",
     init = function()
-      vim.g.minimap_auto_start = 1
-      vim.g.minimap_auto_start_win_enter = 1
+      vim.g.minimap_auto_start = 0
+      vim.g.minimap_auto_start_win_enter = 0
       vim.g.minimap_highlight_search = 1
       vim.g.minimap_width = 20
 
