a35b7f1fb147eb3527606db9b296493664e2ccb9
Anton Kesy <antonkesy@gmail.com>
2024-10-13 10:49:35 +0200

    remove unused c++ configs

--- lua/user/dap/c_cpp_rust.lua
+++ lua/user/dap/c_cpp_rust.lua
@@ -19,36 +19,6 @@ dap.configurations.cpp = {
     args = {},
   },
 }
--- dap.adapters.clangd = {
---   id = 'clangd',
---   type = 'executable',
---   command = 'clangd',
--- }
 
--- dap.configurations.cpp = {
---   {
---     name = "Launch",
---     type = "clangd",
---     request = "launch",
---     program = function()
---       return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
---     end,
---     cwd = '${workspaceFolder}',
---     stopOnEntry = false,
---   },
--- }
 dap.configurations.c = dap.configurations.cpp
 dap.configurations.rust = dap.configurations.cpp
-
--- dap.adapters.codelldb = {
---   type = 'server',
---   port = "${port}",
---   executable = {
---       -- CHANGE THIS to your path!
---       command = '/absolute/path/to/codelldb/extension/adapter/codelldb',
---       args = {"--port", "${port}"},
-
---       -- On windows you may have to uncomment this:
---       -- detached = false,
---     }
--- }
