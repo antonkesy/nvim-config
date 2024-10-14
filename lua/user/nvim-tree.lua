ccd562342070ff2f5b178721d317ed82b343422d
Anton Kesy <antonkesy@gmail.com>
2024-10-14 14:32:55 +0200

    split nvim-tree/tree-sitter

---
+++ lua/user/nvim-tree.lua
@@ -0,0 +1,8 @@
+lvim.builtin.nvimtree.setup.view.side = "left"
+lvim.builtin.nvimtree.setup.renderer.icons.show.git = true
+
+-- Automatically install missing parsers when entering buffer
+lvim.builtin.nvimtree.setup.view.width = 70
+
+lvim.builtin.nvimtree.setup.update_cwd = false
+lvim.builtin.nvimtree.setup.update_focused_file.update_root = false
--- lua/user/tree-sitter.lua
+++ lua/user/tree-sitter.lua
@@ -1,14 +1,5 @@
 lvim.builtin.treesitter.ensure_installed = "all"
-
-lvim.builtin.nvimtree.setup.view.side = "left"
-lvim.builtin.nvimtree.setup.renderer.icons.show.git = true
-
--- Automatically install missing parsers when entering buffer
 lvim.builtin.treesitter.auto_install = true
-lvim.builtin.nvimtree.setup.view.width = 70
-
-lvim.builtin.nvimtree.setup.update_cwd = false
-lvim.builtin.nvimtree.setup.update_focused_file.update_root = false
 
 -- -- Autocommands (`:help autocmd`) <https://neovim.io/doc/user/autocmd.html>
 vim.api.nvim_create_autocmd("FileType", {
