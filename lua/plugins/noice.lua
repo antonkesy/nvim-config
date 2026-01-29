55ee6cc9e77366d83dbe9ee0051c6e330ca14aa6
Anton Kesy <anton@kesy.de>
2026-01-30 00:01:03 +0100

    fix noice call

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -1,9 +1,12 @@
-require("noice").setup({
-  cmdline = {
-    view = "cmdline_popup",
-    opts = {
-      position = { row = "50%", col = "50%", anchor = "c" },
-      size = { width = 60 },
+return {
+  "folke/noice.nvim",
+  opts = {
+    cmdline = {
+      view = "cmdline_popup",
+      opts = {
+        position = { row = "50%", col = "50%", anchor = "c" },
+        size = { width = 60 },
+      },
     },
   },
-})
+}
