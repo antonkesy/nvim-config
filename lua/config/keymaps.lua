0e7a702296ea6268e9b3de9da16f46206177a95c
Anton Kesy <anton@kesy.de>
2026-01-30 23:45:18 +0100

    increase lazygit window size

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,19 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local map = vim.keymap.set
+
+-- lazygit
+if vim.fn.executable("lazygit") == 1 then
+  map("n", "<leader>gg", function()
+    -- luacheck: globals Snacks LazyVim
+    Snacks.lazygit({
+      cwd = LazyVim.root.git(),
+      win = {
+        width = 0.999,
+        height = 0.999,
+        border = "rounded",
+      },
+    })
+  end, { desc = "Lazygit" })
+end
