2b7cdd45c7ada219b498deabe21a7d5df0d2481c
Anton Kesy <anton@kesy.de>
2026-02-20 11:34:59 +0100

    fix harper name

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -2,10 +2,10 @@ return {
   "mason-org/mason-lspconfig.nvim",
   opts = {
     ensure_installed = {
-      "harper-ls",
+      "harper_ls",
     },
     -- handlers = {
-    --   ["harper-ls"] = function()
+    --   ["harper_ls"] = function()
     --     require("lspconfig").harper.setup({
     --       filetypes = { "markdown", "tex" },
     --       settings = {
